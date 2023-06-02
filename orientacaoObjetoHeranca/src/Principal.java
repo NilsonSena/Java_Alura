67815d6508d546b3019fa6de321a2222fb159410
NilsonSena <juninhobh96@hotmail.com>
2023-06-02 19:05:22 -0300

    Backup Estudos

--- orientacaoObjetoHeranca/src/Principal.java
+++ orientacaoObjetoHeranca/src/Principal.java
@@ -1,3 +1,6 @@
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
+import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
+import br.com.alura.screenmatch.modelos.Episodio;
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 
@@ -31,5 +34,31 @@ public class Principal {
         lost.exibeFichaTecnica();
 
         System.out.println("Duração para maratonar serie " + lost.getDuracaoEmMinutos());
+
+        Filme outroFilme = new Filme();
+        outroFilme.setNome("Avatar");
+        outroFilme.setAnoDeLancamento(2023);
+        outroFilme.setDuracaoEmMinutos(200);
+        System.out.println("Duração do filme: " + meuFilme.getDuracaoEmMinutos());
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+
+        calculadora.inclui(meuFilme);
+        calculadora.inclui(outroFilme);
+
+        calculadora.inclui(lost);
+
+        System.out.println(calculadora.getTempoTotal());
+
+        FiltroRecomendacao filtro = new FiltroRecomendacao();
+        filtro.filtra(meuFilme);
+
+        Episodio episodio = new Episodio();
+        episodio.setNumero(1);
+        episodio.setSerie(lost);
+        episodio.setTotalDeVisualizacoes(300);
+
+        filtro.filtra(episodio);
+
     }
 }
